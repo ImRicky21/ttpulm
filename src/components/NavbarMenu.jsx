bd6e7cb9fba829a7e82161f532a061ec11c97899
ImRicky21 <rickyrahmad2121@gmail.com>
2023-10-17 05:50:13 +0800

    new branching

--- src/components/NavbarMenu.jsx
+++ src/components/NavbarMenu.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import logoTP from "../assets/logo-Teknologi-Pendidikan-FKIP-ulm.png"
 export default function NavbarMenu() {
     const [isOpen,setIsOpen] = useState()
     return(
         <nav className={`container flex items-center justify-between flex-wrap p-3 backdrop-blur-xl `}>
             <div className="">
-                <a className="lg:hidden"><Link to="/">logo</Link></a>
+                <h2 className="lg:hidden"><Link to="/"><img src={logoTP} alt="logo-tp" className="w-11" /></Link></h2>
             </div>
             <div className="lg:hidden block">
                 <button onClick={()=> setIsOpen (!isOpen)} className="">
@@ -17,8 +17,8 @@ export default function NavbarMenu() {
             </div>
             <div className={`trasnsition ease-in-out w-full block relative flex-grow justify-between lg:flex lg:items-center lg:w-auto ${isOpen ? "block" : "hidden"}`}>
             <div className="transition ease-in-out flex flex-col lg:flex-row lg:flex-grow ">
-                 <a className=" group mx-3">
-            <a href="#" className="text-slate-950">Profil</a>
+                 <button className=" group mx-3">
+            <h2 href="#" className="text-slate-950">Profil</h2>
             <ul className=" md:absolute transition duration-150 hidden group-hover:block align-middle bg-slate-50 text-gray-800 space-y-2 shadow ">
               <li><Link to="/latar-belakang" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Latar Belakang</Link></li>
               <li><Link to="/kata-sambutan" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Kata Sambutan Koordinator Program Studi</Link></li>
@@ -26,9 +26,9 @@ export default function NavbarMenu() {
               <li><Link to="/struktur-organisasi" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Struktur Organisasi</Link></li>
               <li><Link to="/dosen-dan-tenaga-kependidikan" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Dosen & Tenaga Kependidikan</Link></li>
             </ul>
-                </a>
-                 <a className=" group mx-3">
-            <a href="#" className="text-slate-950">Akademik</a>
+                </button>
+                 <button className=" group mx-3">
+            <h2 href="#" className="text-slate-950">Akademik</h2>
             <ul className="z-40 md:absolute hidden group-hover:block bg-slate-50 text-gray-800 space-y-2 shadow">
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Seminar Hasil/Ujian Skripsi</Link></li>
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Jurnal</Link></li>
@@ -36,35 +36,35 @@ export default function NavbarMenu() {
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Kurikulum</Link></li>
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Jadwal Perkuliahan</Link></li>
             </ul>
-                </a>
-                 <a className=" group mx-3">
-            <a href="#" className="text-slate-950 ">Mahasiswa</a>
+                </button>
+                 <button className=" group mx-3">
+            <h2 href="#" className="text-slate-950 ">Mahasiswa</h2>
             <ul className="z-40 md:absolute hidden group-hover:block bg-slate-50 text-gray-800 space-y-2 shadow">
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Himpunan Mahasiswa</Link></li>
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Daftar Mahasiswa</Link></li>
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Prestasi Mahasiswa</Link></li>
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Panduan</Link></li>
             </ul>
-                </a>
-                 <a className=" group mx-3">
-            <a href="#" className="text-slate-950 ">Alumni</a>
+                </button>
+                 <button className=" group mx-3">
+            <h2 href="#" className="text-slate-950 ">Alumni</h2>
             <ul className="z-40 md:absolute hidden group-hover:block bg-slate-50 text-gray-800 space-y-2 shadow">
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Prospek Lulusan</Link></li>
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Tracer Alumni</Link></li>
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">CDC ULM</Link></li>  
             </ul>
-                </a>
-                 <a className=" group mx-3">
-            <a href="#" className="text-slate-950 ">Informasi</a>
+                </button>
+                 <button className=" group mx-3">
+            <h2 href="#" className="text-slate-950 ">Informasi</h2>
             <ul className="z-40 md:absolute hidden group-hover:block bg-slate-50 text-gray-800 space-y-2 shadow">
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Jadwal Wisuda </Link></li>
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Brosur Program Studi</Link></li>
               <li><Link to="" className="block transition eas-in-out w-52 p-2 duration-200 delay-200 hover:bg-violet-300">Berita dan Pengumuman</Link></li>  
             </ul>
-                </a>
-                 <a className="">
-                    <a href="#" className="text-white "></a>
-                </a>
+                </button>
+                 <button className=" group mx-3">
+                  <h2 className="text-slate-950 "><Link to="/kebijakan">Kebijakan</Link></h2>
+                </button>
             </div>
        <div>
          <button  className="inline-flex items-center bg-violet-900 font-bold border-0 rounded-xl py-2 px-4 text-white">
